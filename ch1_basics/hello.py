1268264c78b3fcb3cfee0493b3a6fd38cf08e815
shivamdurgbuns <shivamdurgbuns@gmail.com>
2019-09-13 20:24:58 -0700

    First day Second Commit

---
+++ ch1_basics/hello.py
@@ -0,0 +1,8 @@
+print('Hello World!')
+print('What is your name?')
+myName=input()
+print("It\'s good to meet you, "+myName)
+print("The lenght of your name is: "+ str(len(myName)))
+print("What is your age?")
+age=int(input())
+print("You will be "+str(age+2)+" in two years.")
